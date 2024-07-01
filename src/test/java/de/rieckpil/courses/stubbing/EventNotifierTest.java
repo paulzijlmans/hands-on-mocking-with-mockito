9de461268d1a3cd4743e2d6f88033fd7f6cc2a1e
Paul Zijlmans <paul.zijlmans@ordina.nl>
2024-07-01 12:25:48 +0200

    Stubbing Method Calls of a Mock

--- src/test/java/de/rieckpil/courses/stubbing/EventNotifierTest.java
+++ src/test/java/de/rieckpil/courses/stubbing/EventNotifierTest.java
@@ -9,6 +9,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class EventNotifierTest {
@@ -21,9 +23,22 @@ class EventNotifierTest {
 
   @Test
   void voidMethodStubbing() {
+//    doThrow(new RuntimeException("Error")).when(eventNotifier).notifyNewUserCreation("duke");
+    Mockito
+      .doNothing()
+      .doThrow(new RuntimeException("test"))
+      .when(eventNotifier).notifyNewUserCreation("duke");
+
+    eventNotifier.notifyNewUserCreation("duke");
+    assertThrows(RuntimeException.class, () -> eventNotifier.notifyNewUserCreation("duke"));
   }
 
   @Test
   void doReturnExample() {
+//    when(bannedUsersClient.amountOfBannedAccounts()).thenReturn(42);
+
+    doReturn(42).when(bannedUsersClient).amountOfBannedAccounts();
+
+    System.out.println(bannedUsersClient.amountOfBannedAccounts());
   }
 }
--- src/test/java/de/rieckpil/courses/stubbing/RegistrationServiceBDDTest.java
+++ src/test/java/de/rieckpil/courses/stubbing/RegistrationServiceBDDTest.java
@@ -31,5 +31,24 @@ public class RegistrationServiceBDDTest {
 
   @Test
   void basicStubbingWithBDD() {
+    BDDMockito
+      .given(userRepository.findByUsername("duke"))
+      .willReturn(new User());
+
+    BDDMockito
+      .given(userRepository.save(any(User.class)))
+      .will(invocation -> {
+        User user = invocation.getArgument(0);
+        user.setId(42L);
+        return user;
+      });
+
+    BDDMockito
+      .given(userRepository.findByUsername("mike"))
+      .willThrow(new RuntimeException("Error in DB"));
+
+    System.out.println(userRepository.findByUsername("duke"));
+    System.out.println(userRepository.save(new User()).getId());
+    assertThrows(RuntimeException.class, () -> System.out.println(userRepository.findByUsername("mike")));
   }
 }
