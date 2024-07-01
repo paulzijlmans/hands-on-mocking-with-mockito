68c3e6bd598561733ddc5762707cbbafa3aaf99a
Paul Zijlmans <paul.zijlmans@ordina.nl>
2024-07-01 12:59:20 +0200

    Verifying the Interaction of a Mock

--- src/test/java/de/rieckpil/courses/verification/RegistrationServiceVerificationTest.java
+++ src/test/java/de/rieckpil/courses/verification/RegistrationServiceVerificationTest.java
@@ -36,13 +36,68 @@ public class RegistrationServiceVerificationTest {
 
   @Test
   void basicVerification() {
+    when(bannedUsersClient.isBanned(eq("duke"), any())).thenReturn(true);
+//    when(bannedUsersClient.amountOfGloballyBannedAccounts()).thenReturn(42L);
+
+    assertThrows(IllegalArgumentException.class,
+      () -> cut.registerUser("duke", Utils.createContactInformation("duke@mockito.org")));
+
+    verify(bannedUsersClient).isBanned(eq("duke"), argThat(address -> address.getCity().equals("Berlin")));
+    verify(bannedUsersClient, times(1)).isBanned(eq("duke"), any(Address.class));
+    verify(bannedUsersClient, atLeastOnce()).isBanned(eq("duke"), any(Address.class));
+    verify(bannedUsersClient, atMost(1)).isBanned(eq("duke"), any(Address.class));
+    verify(bannedUsersClient, never()).bannedUserId();
+
+    verifyNoMoreInteractions(bannedUsersClient, userRepository);
+
+//    verify(bannedUsersClient, description("Nobody checked for Mike")).isBanned(eq("mike"), any(Address.class));
   }
 
   @Test
   void additionalVerificationOptions() {
+    when(bannedUsersClient.isBanned(eq("duke"), any(Address.class))).thenReturn(false);
+    when(userRepository.findByUsername("duke")).thenReturn(null);
+    when(userRepository.save(any(User.class))).thenAnswer(invocation -> {
+      User user = invocation.getArgument(0);
+      user.setId(42L);
+      return user;
+    });
+
+    User user = cut.registerUser("duke", Utils.createContactInformation("duke@mockito.org"));
+
+    assertNotNull(user);
+
+    verify(userRepository).save(any(User.class));
+    verify(userRepository).findByUsername("duke");
+    verify(bannedUsersClient).isBanned(eq("duke"), any(Address.class));
+
+    InOrder inOrder = inOrder(userRepository, bannedUsersClient);
+    inOrder.verify(bannedUsersClient).isBanned(eq("duke"), any(Address.class));
+    inOrder.verify(userRepository).findByUsername("duke");
+    inOrder.verify(userRepository).save(any(User.class));
   }
 
   @Test
   void argumentCaptorsWhenVerifying() {
+    when(bannedUsersClient.isBanned(eq("duke"), any(Address.class))).thenReturn(false);
+    when(userRepository.findByUsername("duke")).thenReturn(null);
+    when(userRepository.save(any(User.class))).thenReturn(new User());
+
+    User user = cut.registerUser("duke", Utils.createContactInformation());
+
+    assertNotNull(user);
+
+    verify(userRepository).save(userArgumentCaptor.capture());
+    verify(bannedUsersClient).isBanned(stringArgumentCaptor.capture(), addressArgumentCaptor.capture());
+    System.out.println(stringArgumentCaptor.getValue());
+    System.out.println(addressArgumentCaptor.getValue());
+
+    User userToStore = userArgumentCaptor.getValue();
+
+    System.out.println(userToStore);
+    assertNotNull(userToStore.getUsername());
+    assertNotNull(userToStore.getCreatedAt());
+    assertTrue(userToStore.getEmail().contains("@myorg.io"));
+    assertNull(userToStore.getId());
   }
 }
